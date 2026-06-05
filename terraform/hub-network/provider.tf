d5951c8258566849d928fcdea6cc36f781851c58
Eric Wong <Eric534903081@gmail.com>
2026-06-05 13:55:26 +0800

    fix: migrated terraform state to aws s3 remote backend

--- terraform/hub-network/provider.tf
+++ terraform/hub-network/provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  # This tells Terraform to store its memory in the AWS S3 Bucket
+  backend "s3" {
+    bucket = "zero-trust-tf-state-eric-0301" # <--- YOUR BUCKET NAME HERE
+    key    = "hub-network/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
